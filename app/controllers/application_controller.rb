f8457220dcbbd0af5d70dfccb8fe0762f91cb33d
hanyikoh <hanyikoh@gmail.com>
2021-12-06 18:46:55 +0800

    joke

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     def hello
-        render html: "Robert 看到了没有！这就是我的masterpiece！！！！"
+        render html: "Robert 如果你看到这句话 证明你太虚伪了"
     end
 end
